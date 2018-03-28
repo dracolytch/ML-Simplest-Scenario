c974a60e07b0b94f36a320555e0cd533d46d29a5
Blake Schreurs <blake.schreurs@jhuapl.edu>
2018-03-27 22:12:06 -0400

    Added Section 5
    Section 5, where we add a floor, and penalties for colliding with the floor

---
+++ Blake-ML/5-Obstacle/FloorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour {
+
+    public ObstacleAgent MyAgent;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject == MyAgent.Marker)
+        {
+            if (col.relativeVelocity.magnitude > MyAgent.CollisionVelocityThreshold)
+            {
+                MyAgent.CollisionHappened();
+            }
+        }
+    }
+}
